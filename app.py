30bcc73059e3a3781069caadcb93bbf296ab00e4
Daniel Eliassen <danieeli@ulrik.uio.no>
2024-05-19 18:28:48 +0200

    Updated with documentation

--- app.py
+++ app.py
@@ -2,13 +2,23 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='static')
 
-@app.route('/')
+# Serve the homepage
+@app.route('/docs/')
 def home():
     return send_from_directory('static/html', 'index.html')
 
-@app.route('/<path:path>')
+# Serve any file from the static directory
+@app.route('/docs/<path:path>')
 def static_files(path):
     return send_from_directory('static/html', path)
+
+@app.route('/')
+def index():
+    return send_from_directory('static/landingpage', 'index.html')
+
+@app.route('/about/')
+def about():
+    return send_from_directory('static/landingpage', 'about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
